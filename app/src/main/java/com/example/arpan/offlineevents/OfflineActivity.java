82532de4d504c486eb7dffb392a8483bf5706cd2
dungarpan <arpanchatterjeeg@gmail.com>
2018-03-01 01:49:20 +0530

    almost everything done except event description and phone nuber and fb

---
+++ app/src/main/java/com/example/arpan/offlineevents/OfflineActivity.java
@@ -0,0 +1,106 @@
+package com.example.arpan.offlineevents;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class OfflineActivity extends AppCompatActivity {
+    public void todesc1(View view){
+        Intent intent = new Intent(getApplicationContext(),desc1.class);
+        startActivity(intent);
+    }
+    public void todesc2(View view){
+        Intent intent = new Intent(getApplicationContext(),desc2.class);
+        startActivity(intent);
+    }
+    public void todesc3(View view){
+        Intent intent = new Intent(getApplicationContext(),desc3.class);
+        startActivity(intent);
+    }
+    public void todesc4(View view){
+        Intent intent = new Intent(getApplicationContext(),desc4.class);
+        startActivity(intent);
+    }
+    public void todesc5(View view){
+        Intent intent = new Intent(getApplicationContext(),desc5.class);
+        startActivity(intent);
+    }
+    public void todesc6(View view){
+        Intent intent = new Intent(getApplicationContext(),desc6.class);
+        startActivity(intent);
+    }
+    public void todesc7(View view){
+        Intent intent = new Intent(getApplicationContext(),desc7.class);
+        startActivity(intent);
+    }
+    public void todesc8(View view){
+        Intent intent = new Intent(getApplicationContext(),desc8.class);
+        startActivity(intent);
+    }
+    public void todesc9(View view){
+        Intent intent = new Intent(getApplicationContext(),desc9.class);
+        startActivity(intent);
+    }
+    public void todesc10(View view){
+        Intent intent = new Intent(getApplicationContext(),desc10.class);
+        startActivity(intent);
+    }
+    public void todesc11(View view){
+        Intent intent = new Intent(getApplicationContext(),desc11.class);
+        startActivity(intent);
+    }
+    public void todesc12(View view){
+        Intent intent = new Intent(getApplicationContext(),desc12.class);
+        startActivity(intent);
+    }
+    public void todesc13(View view){
+        Intent intent = new Intent(getApplicationContext(),desc13.class);
+        startActivity(intent);
+    }
+    public void todesc14(View view){
+        Intent intent = new Intent(getApplicationContext(),desc14.class);
+        startActivity(intent);
+    }
+    public void todesc15(View view){
+        Intent intent = new Intent(getApplicationContext(),desc15.class);
+        startActivity(intent);
+    }
+    public void todesc16(View view){
+        Intent intent = new Intent(getApplicationContext(),desc16.class);
+        startActivity(intent);
+    }
+    public void todesc17(View view){
+        Intent intent = new Intent(getApplicationContext(),desc17.class);
+        startActivity(intent);
+    }
+    public void todesc18(View view){
+        Intent intent = new Intent(getApplicationContext(),desc18.class);
+        startActivity(intent);
+    }
+    public void todesc19(View view){
+        Intent intent = new Intent(getApplicationContext(),desc19.class);
+        startActivity(intent);
+    }
+    public void todesc20(View view){
+        Intent intent = new Intent(getApplicationContext(),desc20.class);
+        startActivity(intent);
+    }
+    public void todesc21(View view){
+        Intent intent = new Intent(getApplicationContext(),desc21.class);
+        startActivity(intent);
+    }
+    public void todesc22(View view){
+        Intent intent = new Intent(getApplicationContext(),desc22.class);
+        startActivity(intent);
+    }
+    public void todesc23(View view){
+        Intent intent = new Intent(getApplicationContext(),desc23.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline);
+    }
+}
